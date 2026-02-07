71a2698a8ea40b2e8eb334081ef640f8aa5e80b7
mrugakshi harkare <141763523+mrugakshiharkare@users.noreply.github.com>
2026-02-07 15:44:01 +0530

    Reformat app.py with consistent spacing and comments

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# S1: Import Flask
+# Import Flask
 from flask import Flask, request
 
-# S2: Initialize object:
+# Initialize object:
 app = Flask(__name__)
 
-# S3: Route:
+# Route:
 # Task: Takes 'name' from query parameter and converts to UPPER CASE
 @app.route('/')
 def home():
@@ -14,7 +14,8 @@ def home():
     
     return f"<h1>Hello, {upper_name}!</h1><p>Welcome to the Upper Case Home Page.</p>"
 
-# S4: Run the Application:
+# Run the Application:
 if __name__ == '__main__':
     app.run(debug = True)
+
 
